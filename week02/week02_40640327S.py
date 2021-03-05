77419b4e0e874a7997eb03a48438d4b4506e3975
jimmycoco <79588094+jimmycoco@users.noreply.github.com>
2021-03-05 17:37:56 +0800

    Add files via upload

---
+++ week02/week02_40640327S.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+def main(inputSTR):
+    '''
+    這支程式的主要函式(「函式」就是「功能」的意思！)
+    '''
+    print("Hello {}, {}".format(inputSTR, "你好"))
+    inputSubSTR1 = inputSTR[0:3] #用 substring 的方式取得字串中的某一部份
+    inputSubSTR2 = inputSTR[4:13]
+    print(inputSubSTR1)
+    print(inputSubSTR2)
+
+    inputSubLIST = inputSTR.split(" ") #用 split() 函式，透過空格把字串切割成列表
+    print(inputSubLIST)
+    messageSTR = """
+    「程式設計與基礎資料型態與中文構詞學」
+    整堂課的資訊量爆炸，在知識的海洋裡衝浪
+    超過癮的啊啊啊啊！
+    """
+    print(messageSTR)
+
+# #字符號後，是 Python 會將之忽略的註解標記
+#程式進入點！ week02.py 這支程式從這裡開始「執行」！
+if __name__ == "__main__":
+    nameSTR = "張皓鈞 40640327S"
+
+    main(nameSTR)
