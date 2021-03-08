e598037c1d9262e72f4aebb72adbd7cdeca18fc6
Eric40840326s <79822701+Eric40840326s@users.noreply.github.com>
2021-03-08 19:10:56 +0800

    Update week02_40840326s.py

--- week02/week02_40840326s.py
+++ week02/week02_40840326s.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
@@ -24,3 +25,31 @@ if __name__ == "__main__":
     nameSTR = "陳瑞廷 40840326s"
 
     main(nameSTR)
+=======
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+def main(inputSTR):
+    '''
+    這支程式的主要函式(「函式」就是「功能」的意思！)
+    '''
+    print("Hello {}, {}".format(inputSTR, "你好"))
+    inputSubSTR = inputSTR[0:9] #用 substring 的方式取得字串中的某一部份
+    print(inputSubSTR)
+
+    inputSubLIST = inputSTR.split(" ") #用 split() 函式，透過空格把字串切割成列表
+    print(inputSubLIST)
+    messageSTR = """
+    「程式設計與基礎資料型態與中文構詞學」
+    整堂課的資訊量爆炸，在知識的海洋裡衝浪
+    超過癮的啊啊啊啊！
+    """
+    print(messageSTR)
+
+# #字符號後，是 Python 會將之忽略的註解標記
+#程式進入點！ week02.py 這支程式從這裡開始「執行」！
+if __name__ == "__main__":
+    nameSTR = "陳瑞廷 40840326s"
+
+    main(nameSTR)
+>>>>>>> Stashed changes
