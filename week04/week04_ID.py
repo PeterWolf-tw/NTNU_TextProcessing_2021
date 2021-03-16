7fe5896505756a23f406ca1d52c3a06dd748a300
PeterWolf <peterwolf.wang@gmail.com>
2021-03-16 11:44:05 +0800

    上傳 week04 作業

---
+++ week04/week04_ID.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+def main(txtFILE):
+    '''
+    藉由給定 txtFILE 的位置，讀取 .txt 純文字檔。
+    '''
+    with open(txtFILE, encoding="utf-8") as f:
+        txtSTR = f.read()
+    return txtSTR
+
+
+if __name__== "__main__":
+
+
+    fileTUPLE = (DogPeople, PeopleDog)
+    txtSTR = main(fileTUPLE[0])
+
+    x = txtSTR.count("婦人")
+    y = txtSTR.count("土狗")
+    z = txtSTR.count("男")
+    dbpLIST = [("婦人", x), ("土狗", y), ("男", z)]
+    print(fileTUPLE[0].split("/")[1], dbpLIST)
+
+
+    resultSTR = main(fileTUPLE[1])
+    xINT = resultSTR.count("婦人")
+    yINT = resultSTR.count("土狗")
+    zINT = resultSTR.count("男")
+    pbdLIST = [("婦人", xINT), ("土狗", yINT), ("男", zINT)]
+    print(fileTUPLE[1].split("/")[1], pbdLIST)
+
+    resultDICT = {"DogPeople":{"婦人":x_int,
+                               "土狗":y_int,
+                               "男": z_int
+                               },
+                  "PeopleDog":{"婦人":x_int,
+                               "土狗":y_int,
+                               "男": z_int
+                               },
+    }
+
+    #請在這一段設計你的程式，讓 resultDICT 內的 x_int, y_int, z_int 都有正確的值
+    # Your Code Here
+
+
+
+
+    #
+    print(resultDICT)
